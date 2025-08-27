cdda855cd2cd5adbd2bc7fcf2b9cffc96d06ee2f
antongolovan <golovan.ant@gmail.com>
2025-08-27 18:17:03 +0300

    Введение в HTTPX. Практическое задание: работа с HTTPX

--- httpx_get_user_me.py
+++ httpx_get_user_me.py
@@ -8,8 +8,7 @@ login_payload = {
 
 base_url = "http://localhost:8000"
 login_url = "/api/v1/authentication/login"
-get_user_info_url = "/api/v1/users/me"
-
+get_user_me_url = "/api/v1/users/me"
 
 login_response = httpx.post(url=base_url + login_url, json=login_payload)
 login_response_data = login_response.json()
@@ -19,11 +18,11 @@ headers = {
     "Authorization": f"Bearer {access_token}"
 }
 
-user_info_response = httpx.get(url=base_url + get_user_info_url, headers=headers)
-user_info_response_data = user_info_response.json()
+get_user_me_response = httpx.get(url=base_url + get_user_me_url, headers=headers)
+get_user_me_response_data = get_user_me_response.json()
 
-pprint(f"Status code: {user_info_response.status_code}", indent=4)
-pprint(f"User info: {user_info_response_data}", indent=4)
+pprint(f"Status code: {get_user_me_response.status_code}", indent=4)
+pprint(f"User info: {get_user_me_response_data}", indent=4)
 
 
 
