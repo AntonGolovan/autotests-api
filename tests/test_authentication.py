a6c7a55780d7dee4f7fea98496e217d785d1fbc5
antongolovan <golovan.ant@gmail.com>
2025-09-05 19:20:45 +0300

    8.5 Фикстуры Pytest. Практикуемся в использовании pytest.mark.usefixtures

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -1,28 +1,29 @@
-import pytest
 from http import HTTPStatus
-from clients.authentication.authentication_client import get_authentication_client
+
+import pytest
+
+from clients.authentication.authentication_client import AuthenticationClient
 from clients.authentication.authentication_schema import LoginRequestSchema, LoginResponseSchema
-from clients.users.public_users_client import get_public_users_client
-from clients.users.users_schema import CreateUserRequestSchema
+from tests.conftest import UserFixture
 from tools.assertions.authentication import assert_login_response
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
 
+
 @pytest.mark.regression
 @pytest.mark.authentication
-def test_login():
-    public_users_client = get_public_users_client()
-    create_user_request = CreateUserRequestSchema()
-    public_users_client.create_user(request=create_user_request)
+def test_login(
+        function_user: UserFixture,  # Используем фикстуру для создания пользователя
+        authentication_client: AuthenticationClient
+):
+    # Запрос на логин (login_request -> request)
+    request = LoginRequestSchema(email=function_user.email, password=function_user.password)
+    # Выполняем логин (login_response -> response)
+    response = authentication_client.login_api(request)
+    # Валидация ответа (login_response_data -> response_data)
+    response_data = LoginResponseSchema.model_validate_json(response.text)
 
-    login_request = LoginRequestSchema(
-        email=create_user_request.email,
-        password=create_user_request.password
-    )
-    authentication_client = get_authentication_client()
-    login_response = authentication_client.login_api(request=login_request)
-    login_response_data = LoginResponseSchema.model_validate_json(login_response.text)
+    assert_status_code(response.status_code, HTTPStatus.OK)
+    assert_login_response(response_data)
 
-    assert_status_code(actual=login_response.status_code, expected=HTTPStatus.OK)
-    assert_login_response(response=login_response_data)
-    validate_json_schema(instance=login_response.json(), schema=login_response_data.model_json_schema())
+    validate_json_schema(response.json(), response_data.model_json_schema())
