5931c17b203b3cce8a783503853487a5f4ba3223
AntonGolovan <golova37rus@gmail.com>
2025-09-28 18:26:44 +0300

    9.4 Практикуемся в написании API автотестов. Часть 3. Практика написания API автотестов для работы с заданиями. Обновление задания

--- tools/assertions/exercises.py
+++ tools/assertions/exercises.py
@@ -3,6 +3,8 @@ from clients.exercises.exercises_schema import (
     CreateExerciseResponseSchema,
     ExerciseSchema,
     GetExerciseResponseSchema,
+    UpdateExerciseRequestSchema,
+    UpdateExerciseResponseSchema,
 )
 from tools.assertions.base import assert_equal
 
@@ -51,3 +53,19 @@ def assert_get_exercise_response(get_exercise_response: GetExerciseResponseSchem
     :raises AssertionError: Если хотя бы одно поле не совпадает
     """
     assert_exercise(actual=get_exercise_response.exercise, expected=create_exercise_response.exercise)
+
+
+def assert_update_exercise_response(request: UpdateExerciseRequestSchema, response: UpdateExerciseResponseSchema):
+    """
+    Проверяет, что ответ на создание задания соответствует ответу на создание задания.
+
+    :param request: Запрос на изменение данных задания.
+    :param response: Ответ API на изменение задания.
+    :raises AssertionError: Если хотя бы одно поле не совпадает
+    """
+    assert_equal(response.exercise.title, request.title, "title")
+    assert_equal(response.exercise.max_score, request.max_score, "max_score")
+    assert_equal(response.exercise.min_score, request.min_score, "min_score")
+    assert_equal(response.exercise.description, request.description, "description")
+    assert_equal(response.exercise.order_index, request.order_index, "order_index")
+    assert_equal(response.exercise.estimated_time, request.estimated_time, "estimated_time")
